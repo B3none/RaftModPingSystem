f0e2186b6f5406948812b33d0112aa4c86a20f37
B3none <ablackham2000@gmail.com>
2020-12-27 16:59:56 +0000

    Added initial RaftMod project structure

---
+++ RaftModPingSystem/RaftModPingSystem.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class RaftModPingSystem : Mod
+{
+    public void Start()
+    {
+        Debug.Log("Mod RaftModPingSystem has been loaded!");
+    }
+
+    public void OnModUnload()
+    {
+        Debug.Log("Mod RaftModPingSystem has been unloaded!");
+    }
+}
